979ca79e348e63ac387522a946d6bd5587547c26
UzzI <59740776+uzziconsulting@users.noreply.github.com>
2023-06-09 19:29:50 -0400

    add vote outline

--- votetokenv0.001.rs
+++ votetokenv0.001.rs
@@ -43,3 +43,44 @@ impl MembershipTokenContract {
         *balance += amount;
     }
 }
+# Vote outline 
+use bulletproofs::{BulletproofGens, PedersenGens, RangeProof, R1CSProof, Verifier};
+use curve25519_dalek::scalar::Scalar;
+use merlin::Transcript;
+
+struct VotingContract {
+    // This would actually be some sort of state on the blockchain
+    votes: Vec<(CompressedRistretto, R1CSProof)>,
+}
+
+impl VotingContract {
+    fn new() -> Self {
+        VotingContract { votes: Vec::new() }
+    }
+
+    fn submit_vote(&mut self, vote_commitment: CompressedRistretto, proof: R1CSProof) {
+        // In a real contract, you would verify the proof before adding the vote
+        self.votes.push((vote_commitment, proof));
+    }
+
+    fn verify_votes(&self) -> bool {
+        let pc_gens = PedersenGens::default();
+        let bp_gens = BulletproofGens::new(64, 1);
+
+        for (commitment, proof) in &self.votes {
+            let mut verifier_transcript = Transcript::new(b"VoteProof");
+            if RangeProof::verify_single(
+                &proof,
+                &bp_gens,
+                &pc_gens,
+                &mut verifier_transcript,
+                &commitment,
+                2,  // The upper limit for our range proof, adjust for your needs
+            ).is_err() {
+                return false;
+            }
+        }
+
+        true
+    }
+}
